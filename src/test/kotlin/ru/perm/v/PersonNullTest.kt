b4ccc13533f56cc76706169c6d932510edda50c1
Черепахин Василий <vasi.che@gmail.com>
2023-06-15 10:39:07 +0500

    demo lateinit

--- src/test/kotlin/ru/perm/v/PersonNullTest.kt
+++ src/test/kotlin/ru/perm/v/PersonNullTest.kt
@@ -3,12 +3,23 @@ package ru.perm.v
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.assertThrows
 
 class PersonNullTest {
 
     @Test
     fun getErrNameLate() {
         val person = PersonNull()
-        assertEquals(1, person.getErrNameLate())
+//kotlin.UninitializedPropertyAccessException: lateinit property nameLate has not been initialized
+//        assertEquals(1, person.getErrNameLate())
+        person.nameLate="-"
+        assertEquals("-", person.getErrNameLate())
+    }
+
+    @Test
+    internal fun nullExcpt() {
+        val person = PersonNull()
+//kotlin.UninitializedPropertyAccessException: lateinit property nameLate has not been initialized
+        assertThrows<UninitializedPropertyAccessException> { person.getErrNameLate() }
     }
 }
