3d5757b05d394a56c4bb945f13e12597e4670d89
Черепахин Василий <vasi.che@gmail.com>
2023-06-15 11:35:09 +0500

    comments lateinit

--- src/main/kotlin/ru/perm/v/PersonNull.kt
+++ src/main/kotlin/ru/perm/v/PersonNull.kt
@@ -12,18 +12,17 @@ class PersonNull {
     // ERROR в idea
     // private var nameSimple1: String = null
 
-    // так НЕ требует инициализации. "lateinit" говорит,
-    // что будет проинициализировано ПОЗЖЕ
+    // "lateinit" НЕ требует инициализации. И говорит, что будет проинициализировано ПОЗЖЕ
     // Но можно получить Runtime Error NPE. См. PersonNullTest.uninitializedPropertyAccessException
     // lateinit var nameLate: String
 
-    // хоть и NULL, но инициализировать все равно нужно
+    // хоть "?" говорит, что можно NULL, но инициализировать все равно нужно
     var nameNull:String? = null
 
     lateinit var nameLate: String
 
     fun getErrNameLate(): String {
-        // будет Runtime Error, если не инициалицировано, а помечено lateinit
+        // При выполнении будет Runtime Error, если не инициалицировано, а помечено lateinit
         return nameLate
     }
 }
